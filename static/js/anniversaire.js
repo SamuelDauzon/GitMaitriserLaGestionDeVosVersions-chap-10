461e3f2dbfe45565d6835a214270add99a5ae56c
Samuel DAUZON <git@dauzon.com>
2015-10-01 21:41:21 +0200

    Proposition des couleurs déjà utilisées

--- static/js/anniversaire.js
+++ static/js/anniversaire.js
@@ -14,6 +14,7 @@ function anniv_ajout() {
     document.querySelector('#anniv_personne').value = "";
     document.querySelector('#anniv_date').value     = "";
     document.querySelector('#couleur').value     = "";
+    charge_couleurs();
     anniv_liste();
 }
 
@@ -40,3 +41,32 @@ function anniv_get_nb_anniv() {
     }
     return nb_anniv;
 }
+
+function charge_couleurs() {
+    nb_anniv = anniv_get_nb_anniv();
+    // On vide la div contenant les couleurs si elle en contient pour recréer les couleurs.
+    couleur_div = document.querySelector('#couleurs_proposees');
+    couleur_div.innerHTML = '';
+
+    couleur_list = Array();
+    for (i=0;i<nb_anniv;i++) {
+        anniversaire = JSON.parse(localStorage.getItem(i.toString()));
+        couleur_list.push(anniversaire.couleur);
+    }
+    couleur_list_unique = couleur_list.filter(function(elem, pos) {
+        return couleur_list.indexOf(elem) == pos;
+    })
+    // Génération et affichage des boutons
+    couleur_list.filter(function(elem, pos) {
+        couleur_div.innerHTML = couleur_div.innerHTML +
+            '<button style="color:#'+elem+'">'+elem+'</button>';
+    })
+    // Ecouteurs de boutons pour remplir la couleur choisie
+    $("#couleurs_proposees button").bind("click", function() {
+        $('#couleur').val($(this).text());
+    });
+}
+
+$(document).ready(function() {
+    charge_couleurs();
+});
